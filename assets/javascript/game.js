3945d48636e85169ce8bbe3accf357f22bbfb6b2
morralex <morr.alex@outlook.com>
2019-06-13 17:59:52 -0700

    indexAll

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,34 @@
 //list the words that user will be guessing
 
-var songs = ["IWantYou", "Blackbird", "ComeTogether", "Something", "PennyLane", "InMyLife", "AHardDaysNight"];
-songs.forEach(function (e) {
-    console.log(e)
+function getAllIndexes(arr, val) {
+    var indexes = [], i = -1;
+    while ((i = arr.indexOf(val, i + 1)) != -1) {
+        indexes.push(i);
+    }
+    return indexes;
+}
 
+var songs = ["IWantYou", "Blackbird", "ComeTogether", "Something", "PennyLane", "InMyLife", "AHardDaysNight"];
+var underScr = ['_', '_', '_']
+var is = getAllIndexes(songs, '')
+songs.forEach(function (i) {
+    songs[i] = underScr[i]
 })
+
+// if (i !== -1) {
+//     underScr[i] = songs[i]
+// }
+
+
+
+// songs.forEach(function (e) {
+//     console.log(e)
+
+// })
 
 // i want to gran a string from my array and i want it to be random
 
-var rdmSong = songs[Math.floor(Math.random()*songs.length)];
+var rdmSong = songs[Math.floor(Math.random() * songs.length)];
 
 console.log(rdmSong)
 
